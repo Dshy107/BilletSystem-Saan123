1506dbdfef014ff8cf68fafe955ef05853dde2c2
Sarasaan Sivaguru <sara360e@edu.easj.dk>
2017-08-30 12:51:03 +0200

    Tilføjet bil klasse Udført Unittest for pris samt køretøj metode

---
+++ BilletLibTests/BilTests.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BilletLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BilletLib.Tests
+{
+	[TestClass()]
+	public class BilTests
+	{
+		[TestMethod()]
+		public void PrisTestbil()
+		{
+			//Arrange
+			var Bil = new Bil();
+
+			//Act
+			int pris = Bil.Pris();
+
+			//Assert
+			Assert.AreEqual(240, pris);
+
+		}
+
+		[TestMethod()]
+		public void KøretøjTestbil()
+		{
+			//Arrange
+			var Bil = new Bil();
+
+			//Act
+			string køretøj = Bil.Køretøj();
+
+			//Assert
+			Assert.AreEqual("bil", køretøj);
+		}
+	}
+}
