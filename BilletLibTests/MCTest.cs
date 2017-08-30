bc4a595d7c3a516950ffe76f27ba7bc9e5509846
Sarasaan Sivaguru <sara360e@edu.easj.dk>
2017-08-30 13:16:23 +0200

    Oprettet ny klasse "MC" Foretaget Unit Test

---
+++ BilletLibTests/MCTest.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BilletLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BilletLib.Tests
+{
+	[TestClass()]
+	public class MCTests
+	{
+		[TestMethod()]
+		public void PrisTestMC()
+		{
+			//Arrange
+			var MC = new MC();
+
+			//Act
+			int pris = MC.Pris();
+
+			//Assert
+			Assert.AreEqual(125, pris);
+
+		}
+
+		[TestMethod()]
+		public void KøretøjTestMC()
+		{
+			//Arrange
+			var MC = new MC();
+
+			//Act
+			string køretøj = MC.Køretøj();
+
+			//Assert
+			Assert.AreEqual("MC", køretøj);
+		}
+	}
+}
